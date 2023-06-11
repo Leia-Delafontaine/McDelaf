3a776112fd79305600a4dde2b68943e22207f485
Leia Delafontaine <leiadelaf@gmail.com>
2023-06-11 18:27:17 +0200

    Modify : Modification du menu MCDelaf. Amélioration et ajout d'une saisie.

--- src/main.sh
+++ src/main.sh
@@ -1,25 +1,96 @@
 #!/bin/bash
 
-echo 'Bonjour ! Bienvenue chez McDelaf, service de sandwicherie à emporter ! Voici nos sandwiches :
+#Functions to display sandwiches and drinks
 
-jambon beurre
-jambon fromage
-poulet crudités
-thon oeuf
+display(){
+    echo $1;
+}
 
-'
-read -p "Veuillez choisir votre sandwich:  " sandwich
+displayDishes(){
+    echo 'Bonjour ! Bienvenue chez McDelaf, le service de sandwicherie à emporter';
+    echo 'Voici nos sandwiches :';
+    display '1: jambon beurre'
+    display '2: poulet crudités'
+    display '3: oeufs mayonnaise'
 
-echo 'Voici nos boissons :
+}
 
-eau
-soda
-jus de fruits
+displayDrinks(){
+    echo 'Voici nos boissons :'
+    display '1: soda'
+    display '2: eau gazeuse'
+    display '3: eau minérale'
+}
 
-'
 
-read -p "Veullez choisir une boisson:   " boisson
+#Loop to choose a sandwich
 
-echo 'Récapitulatif de votre commande:  '
-echo "sandwich $sandwich et $boisson"
+while true
+do
+    clear;
+    displayDishes;
 
+    read -p "Veuillez choisir votre sandwich:  " dish
+
+    if [ $dish -le 3 ]
+    then
+        echo 'Merci';
+        sleep 1;
+        break;
+    fi
+
+    echo 'Veuillez choisir parmi le choix 1, 2 ou 3';
+    sleep 1;
+done
+
+
+#Loop to choose a drink
+
+while true
+do
+    clear;
+    displayDrinks;
+
+    read -p "Veuillez choisir votre boisson : " drink
+
+    if [ $drink -le 3 ]
+    #mémo perso : solution pour un nombre compris dans une fourchette   [ $drink -lt 7 ] && [ $drink -gt 3 ]
+    then
+        echo 'Merci';
+        sleep 1;
+        break;
+    fi
+
+    echo 'veuillez choisir parmi le choix 1, 2 ou 3';
+    sleep 1;
+done
+
+clear
+
+#To give a recap of the order
+
+display "Voici le récapitulatif de votre menu :"
+
+case $dish in
+    1)
+        display 'Jambon beurre'
+        ;;
+    2)
+        display 'poulet crudités'
+        ;;
+    3)
+        display 'oeufs mayonnaise'
+        ;;
+esac
+
+case $drink in
+    1)
+        display 'soda'
+        ;;
+    2)
+        display 'eau gazeuse'
+        ;;
+    3)
+        display 'eau minérale'
+        ;;
+esac
